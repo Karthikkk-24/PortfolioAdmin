44855eb9dee504e44fdcfe086165712658f0281e
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-08-30 22:34:19 +0530

    Added PublicRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import PublicRoute from './components/PublicRoute';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 
@@ -8,9 +9,10 @@ function App() {
         <>
             <Router>
                 <Routes>
-                    <Route path="/" element={<Dashboard />} />
                     <Route path="/login" element={<Login />} />
-                    <Route path="/main" element={<Login />} />
+                    <Route path="/" element={<PublicRoute />}>
+                        <Route path="/dashboard" element={<Dashboard />} />
+                    </Route>
                 </Routes>
             </Router>
         </>
