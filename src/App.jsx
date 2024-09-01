91fdf568a87114e86598fc6d82f90d40778e9fb6
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-09-01 15:54:27 +0530

    Projects, Blogs Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import PublicRoute from './components/PublicRoute';
+import Blogs from './pages/Blogs';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
+import Projects from './pages/Projects';
 import Register from './pages/Register';
 
 function App() {
@@ -13,7 +15,10 @@ function App() {
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="/" element={<PublicRoute />}>
+                        <Route path="/" element={<Dashboard />} />
                         <Route path="/dashboard" element={<Dashboard />} />
+                        <Route path="/projects" element={<Projects />} />
+                        <Route path="/blogs" element={<Blogs />} />
                     </Route>
                 </Routes>
             </Router>
